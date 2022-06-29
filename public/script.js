cc5c25997036fe9797674910740cbaa09b62daed
Mila <milashaohuibergmann@gmail.com>
2022-06-29 10:09:43 +0200

    petition finalcheck

--- public/script.js
+++ public/script.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById("signature-pad");
 function resizeCanvas() {
     const ratio = Math.max(window.devicePixelRatio || 1, 1);
-    //canvas.width = canvas.offsetWidth * ratio;
-    //canvas.height = canvas.offsetHeight * ratio;
+    canvas.width = canvas.offsetWidth * ratio;
+    canvas.height = canvas.offsetHeight * ratio;
     canvas.getContext("2d").scale(ratio, ratio);
 }
 window.onresize = resizeCanvas;
@@ -15,12 +15,8 @@ document.getElementById("clear").addEventListener("click", function () {
     signaturePad.clear();
 });
 document.addEventListener("mouseup", () => {
-    console.log(signaturePad.toDataURL())
-  let  dataURL = signaturePad.toDataURL();
-  console.log("dataurl",dataURL)
-  document.getElementById("sign").value = dataURL;
+    console.log(signaturePad.toDataURL());
+    let dataURL = signaturePad.toDataURL();
+    console.log("dataurl", dataURL);
+    document.getElementById("sign").value = dataURL;
 });
-
-
-
-// asign the dataURL to input #sign
